25850d640c700e2b7d9135a21923662931d8f2fd
Akshar <aksharpatel@microsoft.com>
2023-04-21 16:41:00 -0400

    reverting back Binder.cs changes

--- src/libraries/Microsoft.PowerFx.Core/Binding/Binder.cs
+++ src/libraries/Microsoft.PowerFx.Core/Binding/Binder.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation.
+﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
 using System;
@@ -399,19 +399,6 @@ namespace Microsoft.PowerFx.Core.Binding
         {
             features ??= Features.None;
             return Run(glue, null, new DataSourceToQueryOptionsMap(), node, resolver, bindingConfig, false, ruleScope, false, null, features);
-        }
-
-        public static TexlBinding Run(
-           IBinderGlue glue,
-           IExternalRuleScopeResolver externalRuleScopeResolver,
-           TexlNode node,
-           INameResolver resolver,
-           BindingConfig bindingConfig,
-           DType ruleScope,
-           Features features = null)
-        {
-            features ??= Features.None;
-            return Run(glue, externalRuleScopeResolver, new DataSourceToQueryOptionsMap(), node, resolver, bindingConfig, false, ruleScope, false, null, features);
         }
 
         public void WidenResultType()
